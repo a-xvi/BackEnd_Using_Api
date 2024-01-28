f8093da51f39187195a1c8b0e95be22c112893ce
Zero <4.algarwi@gmail.com>
2024-01-28 14:26:26 +0300

    string message

--- app.js
+++ app.js
@@ -7,10 +7,3 @@ mongoose.connect('mongodb://localhost:27017/todo', {
     useUnifiedTopology: true
 });
 
-
-const schema = new mongoose.Schema({ title: String });
-const Task = mongoose.model('Task', schema);
-
-//insert
-const firsttask = new Task({ title: 'writing insert function' });
-firsttask.save().then(() => console.log('new record inserted'));

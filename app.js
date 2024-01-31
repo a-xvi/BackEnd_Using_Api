9eb679867878af83737232f67ac5e84005906d3a
Zero <4.algarwi@gmail.com>
2024-01-31 12:39:54 +0300

    spaces

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ mongoose.connect('mongodb://localhost:27017/todo', {
 });
 
 
-
+//stuff that will be inside the table 
 let schema = new mongoose.Schema({ // schema dont touch
     title: { type: String, required: true },
     time: { type: Number, required: true }
@@ -21,25 +21,31 @@ const Task = mongoose.model('this', schema); // class maybe
 
 
 app.get('/task', (req, res) => {
+    //.find({}) /({}) means find every thing
     Task.find({}).then((tasks) => {
         response = {
             all_tasks: tasks
         }
         res.status(200).json(response)
+
     }).catch((error) => {
         res.json(error).status(404)
     })
-});
 
+});
+// creating the table
 app.post('/task', (req, res) => {
     let newTask = {
         title: req.body.title,
         time: req.body.time
     };
+    const {time,date} = req.body;
 
     let Ftask = new Task(newTask);
+
     Ftask.save().then(() => {
         res.json({ 'task': Ftask });
+
     }).catch(err => {
         res.status(500).json(err);
     })
@@ -55,10 +61,12 @@ app.post('/task', (req, res) => {
 // });
 
 app.get('/task/:id', (req, res) => {
+
     Task.findById(req.params.id).then((task) => {
         if (!task) res.status(404).json({ "message": "not found" })
 
         res.status(200).json(task)
+
     }).catch((error) => {
         res.json(error).status(404)
     })
@@ -66,6 +74,7 @@ app.get('/task/:id', (req, res) => {
 
 
 app.delete('/task/:id', (req, res) => {
+
     Task.findByIdAndDelete(req.params.id).then(() => {
         res.status(200).json({ "message": "deleted" })
 
@@ -74,10 +83,14 @@ app.delete('/task/:id', (req, res) => {
     })
 })
 
+//updating the table
 app.put('/task/:id', (req, res) => {
+
     box = { "title": req.body.title };
+    //                      (where to edit, the edit that should be)
     Task.findByIdAndUpdate(req.params.id, box).then(() => {
         res.json({ "message": "done" })
+
     }).catch(err => {
         res.status(403).json({ "message": err })
     })
